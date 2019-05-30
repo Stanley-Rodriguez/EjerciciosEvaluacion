224ca0feca2e37644f7d905be82fae810dc91437
Stanley Rodriguez <stanley_javier023@outlook.com>
2019-05-30 00:21:49 -0400

    Initial commit

---
+++ EjerciciosEvaluacion/Form1.Designer.cs
@@ -0,0 +1,196 @@
+﻿namespace EjerciciosEvaluacion
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TxtVTMultiplicar = new System.Windows.Forms.TextBox();
+            this.BtnTMultiplicar = new System.Windows.Forms.Button();
+            this.BtnSecuencia = new System.Windows.Forms.Button();
+            this.TxtVSecuencia = new System.Windows.Forms.TextBox();
+            this.BtnOrdenAleatorio = new System.Windows.Forms.Button();
+            this.TxtVOrdenAleatorio = new System.Windows.Forms.TextBox();
+            this.BtnBilletes = new System.Windows.Forms.Button();
+            this.TxtVBilletes = new System.Windows.Forms.TextBox();
+            this.LbxTMultiplicar = new System.Windows.Forms.ListBox();
+            this.LbSecuencia = new System.Windows.Forms.ListBox();
+            this.LbxOrdenAleatorio = new System.Windows.Forms.ListBox();
+            this.LbxBilletes = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // TxtVTMultiplicar
+            // 
+            this.TxtVTMultiplicar.Location = new System.Drawing.Point(13, 29);
+            this.TxtVTMultiplicar.Name = "TxtVTMultiplicar";
+            this.TxtVTMultiplicar.Size = new System.Drawing.Size(147, 20);
+            this.TxtVTMultiplicar.TabIndex = 0;
+            // 
+            // BtnTMultiplicar
+            // 
+            this.BtnTMultiplicar.Location = new System.Drawing.Point(13, 65);
+            this.BtnTMultiplicar.Name = "BtnTMultiplicar";
+            this.BtnTMultiplicar.Size = new System.Drawing.Size(147, 23);
+            this.BtnTMultiplicar.TabIndex = 1;
+            this.BtnTMultiplicar.Text = "Tabla Multiplicar";
+            this.BtnTMultiplicar.UseVisualStyleBackColor = true;
+            this.BtnTMultiplicar.Click += new System.EventHandler(this.BtnTMultiplicar_Click);
+            // 
+            // BtnSecuencia
+            // 
+            this.BtnSecuencia.Location = new System.Drawing.Point(242, 65);
+            this.BtnSecuencia.Name = "BtnSecuencia";
+            this.BtnSecuencia.Size = new System.Drawing.Size(147, 23);
+            this.BtnSecuencia.TabIndex = 3;
+            this.BtnSecuencia.Text = "Secuencia 0.1.1";
+            this.BtnSecuencia.UseVisualStyleBackColor = true;
+            this.BtnSecuencia.Click += new System.EventHandler(this.BtnSecuencia_Click);
+            // 
+            // TxtVSecuencia
+            // 
+            this.TxtVSecuencia.Location = new System.Drawing.Point(242, 29);
+            this.TxtVSecuencia.Name = "TxtVSecuencia";
+            this.TxtVSecuencia.Size = new System.Drawing.Size(147, 20);
+            this.TxtVSecuencia.TabIndex = 2;
+            // 
+            // BtnOrdenAleatorio
+            // 
+            this.BtnOrdenAleatorio.Location = new System.Drawing.Point(472, 65);
+            this.BtnOrdenAleatorio.Name = "BtnOrdenAleatorio";
+            this.BtnOrdenAleatorio.Size = new System.Drawing.Size(147, 23);
+            this.BtnOrdenAleatorio.TabIndex = 5;
+            this.BtnOrdenAleatorio.Text = "Orden aleatorio";
+            this.BtnOrdenAleatorio.UseVisualStyleBackColor = true;
+            this.BtnOrdenAleatorio.Click += new System.EventHandler(this.BtnOrdenAleatorio_Click);
+            // 
+            // TxtVOrdenAleatorio
+            // 
+            this.TxtVOrdenAleatorio.Location = new System.Drawing.Point(472, 29);
+            this.TxtVOrdenAleatorio.Name = "TxtVOrdenAleatorio";
+            this.TxtVOrdenAleatorio.Size = new System.Drawing.Size(147, 20);
+            this.TxtVOrdenAleatorio.TabIndex = 4;
+            // 
+            // BtnBilletes
+            // 
+            this.BtnBilletes.Location = new System.Drawing.Point(703, 65);
+            this.BtnBilletes.Name = "BtnBilletes";
+            this.BtnBilletes.Size = new System.Drawing.Size(147, 23);
+            this.BtnBilletes.TabIndex = 7;
+            this.BtnBilletes.Text = "Desgloce billetes";
+            this.BtnBilletes.UseVisualStyleBackColor = true;
+            this.BtnBilletes.Click += new System.EventHandler(this.BtnBilletes_Click);
+            // 
+            // TxtVBilletes
+            // 
+            this.TxtVBilletes.Location = new System.Drawing.Point(703, 29);
+            this.TxtVBilletes.Name = "TxtVBilletes";
+            this.TxtVBilletes.Size = new System.Drawing.Size(147, 20);
+            this.TxtVBilletes.TabIndex = 6;
+            // 
+            // LbxTMultiplicar
+            // 
+            this.LbxTMultiplicar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LbxTMultiplicar.FormattingEnabled = true;
+            this.LbxTMultiplicar.ItemHeight = 16;
+            this.LbxTMultiplicar.Location = new System.Drawing.Point(13, 107);
+            this.LbxTMultiplicar.Name = "LbxTMultiplicar";
+            this.LbxTMultiplicar.Size = new System.Drawing.Size(196, 420);
+            this.LbxTMultiplicar.TabIndex = 8;
+            // 
+            // LbSecuencia
+            // 
+            this.LbSecuencia.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LbSecuencia.FormattingEnabled = true;
+            this.LbSecuencia.ItemHeight = 16;
+            this.LbSecuencia.Location = new System.Drawing.Point(242, 107);
+            this.LbSecuencia.Name = "LbSecuencia";
+            this.LbSecuencia.Size = new System.Drawing.Size(196, 420);
+            this.LbSecuencia.TabIndex = 9;
+            // 
+            // LbxOrdenAleatorio
+            // 
+            this.LbxOrdenAleatorio.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LbxOrdenAleatorio.FormattingEnabled = true;
+            this.LbxOrdenAleatorio.ItemHeight = 16;
+            this.LbxOrdenAleatorio.Location = new System.Drawing.Point(472, 107);
+            this.LbxOrdenAleatorio.Name = "LbxOrdenAleatorio";
+            this.LbxOrdenAleatorio.Size = new System.Drawing.Size(196, 420);
+            this.LbxOrdenAleatorio.TabIndex = 10;
+            // 
+            // LbxBilletes
+            // 
+            this.LbxBilletes.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LbxBilletes.FormattingEnabled = true;
+            this.LbxBilletes.ItemHeight = 16;
+            this.LbxBilletes.Location = new System.Drawing.Point(703, 107);
+            this.LbxBilletes.Name = "LbxBilletes";
+            this.LbxBilletes.Size = new System.Drawing.Size(196, 420);
+            this.LbxBilletes.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(924, 567);
+            this.Controls.Add(this.LbxBilletes);
+            this.Controls.Add(this.LbxOrdenAleatorio);
+            this.Controls.Add(this.LbSecuencia);
+            this.Controls.Add(this.LbxTMultiplicar);
+            this.Controls.Add(this.BtnBilletes);
+            this.Controls.Add(this.TxtVBilletes);
+            this.Controls.Add(this.BtnOrdenAleatorio);
+            this.Controls.Add(this.TxtVOrdenAleatorio);
+            this.Controls.Add(this.BtnSecuencia);
+            this.Controls.Add(this.TxtVSecuencia);
+            this.Controls.Add(this.BtnTMultiplicar);
+            this.Controls.Add(this.TxtVTMultiplicar);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox TxtVTMultiplicar;
+        private System.Windows.Forms.Button BtnTMultiplicar;
+        private System.Windows.Forms.Button BtnSecuencia;
+        private System.Windows.Forms.TextBox TxtVSecuencia;
+        private System.Windows.Forms.Button BtnOrdenAleatorio;
+        private System.Windows.Forms.TextBox TxtVOrdenAleatorio;
+        private System.Windows.Forms.Button BtnBilletes;
+        private System.Windows.Forms.TextBox TxtVBilletes;
+        private System.Windows.Forms.ListBox LbxTMultiplicar;
+        private System.Windows.Forms.ListBox LbSecuencia;
+        private System.Windows.Forms.ListBox LbxOrdenAleatorio;
+        private System.Windows.Forms.ListBox LbxBilletes;
+    }
+}
+
---
+++ EjerciciosEvaluacion/Form1.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EjerciciosEvaluacion
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //Ejercicio #1
+        private void BtnTMultiplicar_Click(object sender, EventArgs e)
+        {
+            LbxTMultiplicar.Items.Clear();
+
+            int valorTxtBox = Convert.ToInt32(TxtVTMultiplicar.Text);
+            int producto;
+
+            string linea;
+
+            //Este bucle es para el multiplicando y se repetira hasra que llegue al valor
+            //de la tabla deseada
+            for (int multiplicando = 1; multiplicando <= valorTxtBox; multiplicando++) 
+            {
+                //Este bucle es para el multiplicador que realizara la multiplicacion
+                //por el multiplicando 12 veces y luego se agrega por linea al listbox
+                for (int multiplicador = 1; multiplicador < 13; multiplicador++)
+                {
+                    producto = multiplicando * multiplicador;
+                    linea = ""+ multiplicador + "  x  "+ multiplicando + "  =  "+ producto +"";
+                    LbxTMultiplicar.Items.Add(linea);
+                }
+            }
+        }
+
+        //Ejercicio #2
+        private void BtnSecuencia_Click(object sender, EventArgs e)
+        {
+            LbSecuencia.Items.Clear();
+            
+            //Este ejercicio es una serie Fibonacci para mostrar la cantidad
+            //de números deseados de la serie
+            int i = 0;
+            int limiteFb = Convert.ToInt32(TxtVSecuencia.Text);
+            
+            int contenedor; 
+            int valor1 = 0;
+            int valor2 = 1;
+
+            while (i < limiteFb)
+            {
+                LbSecuencia.Items.Add(valor1.ToString()); //Automaticamente agrega el 0 a la lista
+
+                contenedor = valor1;                      //Almacena el valor alternado entre el valor1 y valor2
+                valor1 = valor2;
+                valor2 = contenedor + valor1;             //Almacena el valor de la suma que inicialmente es
+                                                          //0 + 1 = 1 
+                i++;
+            }
+        }
+
+        //Ejercicio 3; Este ejercicio no funciona bien, incluso por mas que investigue por internet no
+        //pude hacerlo. Tiene expresiones lambda, algo que no se manejar bien.
+        private void BtnOrdenAleatorio_Click(object sender, EventArgs e)
+        {
+            LbxOrdenAleatorio.Items.Clear();
+
+            List<int> serie = new List<int>();
+
+            int valor = Convert.ToInt32(TxtVOrdenAleatorio.Text);
+            int numero;
+
+            Random random = new Random(); 
+
+            for (int i = 1; i <= valor; i++)
+            {
+                //el metodo next de la clase random proporciona un valor
+                //valor aleatorio entre dos numeros.
+                numero = random.Next(1, valor);
+
+                //si no existe en la lista declarada se agrega para que 
+                //que no se repita
+                if (!serie.Any(x => x == numero)) 
+                {
+                    serie.Add(numero);
+                }
+            }
+
+            foreach (var item in serie)
+            {
+                LbxOrdenAleatorio.Items.Add(item);
+            }
+
+            //Mi problema con este ejercicio es que solo se agrega a la lista segun el numero de 
+            //repeticiones indicada por la cantidad del texbox, y si hay repetidos no se va a agregar para 
+            //luego ser mostrada. Eso hace que me falten varios numeros y tampoco se agregue el numero mas alto
+        }
+
+        //Ejercicio 4, en este ejercicio utilice un struct para realizar la operacion y obtener el valor
+        //de las variables desde el struct. Traté de hacerlo con un bucle while, pero no pude resolverlo.
+        private void BtnBilletes_Click(object sender, EventArgs e)
+        {
+            LbxBilletes.Items.Clear();
+
+            Operacion.Cantidad = Convert.ToInt32(TxtVBilletes.Text);
+
+            LbxBilletes.Items.Add("Cantidad x Billete");
+
+
+            if (Operacion.Cantidad >= 2000)
+            {
+                Operacion.Calculo(Operacion.Cantidad, 2000);
+
+                LbxBilletes.Items.Add(Operacion.Resultado);
+
+                Operacion.Cantidad = Operacion.Cantidad -= Operacion.Producto;
+            }
+            if (Operacion.Cantidad >= 1000)
+            {
+                Operacion.Calculo(Operacion.Cantidad, 1000);
+
+                LbxBilletes.Items.Add(Operacion.Resultado);
+
+                Operacion.Cantidad = Operacion.Cantidad -= Operacion.Producto;
+            }
+            if (Operacion.Cantidad >= 500)
+            {
+                Operacion.Calculo(Operacion.Cantidad, 500);
+
+                LbxBilletes.Items.Add(Operacion.Resultado);
+
+                Operacion.Cantidad = Operacion.Cantidad -= Operacion.Producto;
+            }
+            if (Operacion.Cantidad >= 200)
+            {
+                Operacion.Calculo(Operacion.Cantidad, 200);
+
+                LbxBilletes.Items.Add(Operacion.Resultado);
+
+                Operacion.Cantidad = Operacion.Cantidad -= Operacion.Producto;
+            }
+            if (Operacion.Cantidad >= 100)
+            {
+                Operacion.Calculo(Operacion.Cantidad, 100);
+
+                LbxBilletes.Items.Add(Operacion.Resultado);
+
+                Operacion.Cantidad = Operacion.Cantidad -= Operacion.Producto;
+            }
+            if (Operacion.Cantidad >= 50)
+            {
+                Operacion.Calculo(Operacion.Cantidad, 50);
+
+                LbxBilletes.Items.Add(Operacion.Resultado);
+
+                Operacion.Cantidad = Operacion.Cantidad -= Operacion.Producto;
+            }
+            if (Operacion.Cantidad >= 25)
+            {
+                Operacion.Calculo(Operacion.Cantidad, 25);
+
+                LbxBilletes.Items.Add(Operacion.Resultado);
+
+                Operacion.Cantidad = Operacion.Cantidad -= Operacion.Producto;
+            }
+            if (Operacion.Cantidad >= 10)
+            {
+                Operacion.Calculo(Operacion.Cantidad, 10);
+
+                LbxBilletes.Items.Add(Operacion.Resultado);
+
+                Operacion.Cantidad = Operacion.Cantidad -= Operacion.Producto;
+            }
+            if (Operacion.Cantidad >= 5)
+            {
+                Operacion.Calculo(Operacion.Cantidad, 5);
+
+                LbxBilletes.Items.Add(Operacion.Resultado);
+
+                Operacion.Cantidad = Operacion.Cantidad -= Operacion.Producto;
+            }
+            if (Operacion.Cantidad >= 1)
+            {
+                Operacion.Calculo(Operacion.Cantidad, 1);
+
+                LbxBilletes.Items.Add(Operacion.Resultado);
+
+                Operacion.Cantidad = Operacion.Cantidad -= Operacion.Producto;
+            }
+        }
+
+        struct Operacion
+        {
+            public static string Resultado;
+            public static int Divisor;
+            public static int Producto;
+            public static int Cantidad;
+
+            public static void Calculo(int cantidad,int dividendo)
+            {
+                Divisor = cantidad / dividendo;
+                Producto = Divisor * dividendo;
+                Resultado = "" + Divisor + " x " + dividendo + " = " + Producto + "";
+            }
+        }
+    }
+}
